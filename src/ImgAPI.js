db7eb34eca5e184c9d90e630f46c2c97015b1765
filinger <mathematics.max@gmail.com>
2018-07-25 14:50:52 +0300

    Update Imgur client id

--- src/ImgAPI.js
+++ src/ImgAPI.js
@@ -2,7 +2,7 @@ export class ImgAPI {
   static fetch = requestUrl => {
     return fetch('https://api.imgur.com/3/' + requestUrl, {
       headers: {
-        Authorization: `Client-ID a7e081ef1fdf64e`
+        Authorization: `Client-ID 8d78ae8b814469f`
       }
     }).then(r => r.json())
   };
